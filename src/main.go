6584d2f751e57c9523afb687df21733f7e6f07bf
Clément Chausson <clement.chausson@epitech.eu>
2024-04-25 12:45:17 +0200

    [~] Fix docker build

--- src/main.go
+++ src/main.go
@@ -17,6 +17,6 @@ func main() {
 	e.GET("/introduce", handler.IntroduceHandler)
 	e.GET("/ask", handler.AskHandler)
 	e.GET("/history", handler.HistoryHandler)
-	e.POST("/text-to-speech", handler.TextToSpeechHandler)
+	e.GET("/text-to-speech", handler.TextToSpeechHandler)
 	e.Logger.Fatal(e.Start(":8080"))
 }
